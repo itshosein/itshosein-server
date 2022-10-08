3dac219fd2b724633812c6bf5296d95812e1b9a2
Hosein Fathi <hfathi711@gmail.com>
2022-10-09 02:45:14 +0330

    added about me paragraph

--- src/components/about-me/index.tsx
+++ src/components/about-me/index.tsx
@@ -13,8 +13,14 @@ function AboutMe() {
       <Typography variant="h3" component="h2" textAlign="center" mb="4rem">
         Who am I ?
       </Typography>
-      <Typography variant="body1" component="h3">
-        Who am I ?
+      <Typography variant="h6" component="h3">
+        I am 25 years old software engineer that lives in Iran. I started my
+        journey as a programmer since four years ago at university where I
+        encountered c++ and get in love with coding, since then I learned the
+        fundamentals of programming and started my front-end career with lots of
+        courses online from Html to CSS and React.js.
+        <br /> I started to work in a real company in February 2020 as a React
+        developer in Setare Aval that is an Iranian financial app.
       </Typography>
     </Box>
   );
