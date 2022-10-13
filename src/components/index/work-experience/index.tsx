5414a3bf1f7236d7acb1484206c81534c6d0f521
Hosein Fathi <hfathi711@gmail.com>
2022-10-13 18:18:49 +0330

    added animation to work experience

--- src/components/index/work-experience/index.tsx
+++ src/components/index/work-experience/index.tsx
@@ -7,15 +7,23 @@ import {
   TimelineContent,
   TimelineDot,
 } from "@mui/lab";
+import { useInView } from "react-intersection-observer";
 
 function WorkExperience() {
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
   return (
     <Box
+      ref={ref}
       component="section"
       sx={{
         width: "100%",
         margin: "4rem 0",
         padding: "2rem 0",
+        transition: "all 1s",
+        transform: `translateX(${inView ? "0" : "-100px"})`,
+        opacity: inView ? "1" : "0",
       }}
     >
       <Typography variant="h4" component="h2" textAlign="center" mb="4rem">
