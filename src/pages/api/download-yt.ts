e99ffd8b45bf7f9fff133953cb4b8d0efe060418
Hosein Fathi <hfathi711@gmail.com>
2022-12-10 23:07:49 +0330

    changed location to save

--- src/pages/api/download-yt.ts
+++ src/pages/api/download-yt.ts
@@ -24,14 +24,12 @@ export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
         ytdl(url, {
           format: selectedFormat ? selectedFormat : undefined,
         })
-          .pipe(fs.createWriteStream(`./public/${name}.mp4`))
+          .pipe(fs.createWriteStream(`./public/yt/${name}.mp4`))
           .on("finish", () => {
-            res
-              .status(200)
-              .json({
-                description: `file created with name=> ${name}`,
-                formatFound: selectedFormat.qualityLabel,
-              });
+            res.status(200).json({
+              description: `file created with name=> ${name}`,
+              formatFound: selectedFormat.qualityLabel,
+            });
           });
       });
     });
