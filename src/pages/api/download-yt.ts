d5578f032f3110069498e9e3fa9c77fcca2c48c5
Hosein Fathi <hfathi711@gmail.com>
2022-12-10 23:22:52 +0330

    added second err handler

--- src/pages/api/download-yt.ts
+++ src/pages/api/download-yt.ts
@@ -4,38 +4,46 @@ import fs from "fs";
 
 type Data = {
   description: string;
-  formatFound: string;
+  formatFound?: string;
   err?: Error;
 };
 
 export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   if (req.query.v) {
     const url = `http://www.youtube.com/watch?v=${req.query.v}`;
-    ytdl.getBasicInfo(url).then((value) => {
-      const name = `${value.videoDetails.title}`;
-      ytdl.getInfo(req.query.v as string).then((info) => {
-        let selectedFormat: ytdl.videoFormat = info.formats.filter(
-          (format) => format.qualityLabel.includes("720") && format.hasAudio
-        )[0];
+    ytdl
+      .getBasicInfo(url)
+      .then((value) => {
+        const name = `${value.videoDetails.title}`;
+        ytdl.getInfo(req.query.v as string).then((info) => {
+          let selectedFormat: ytdl.videoFormat = info.formats.filter(
+            (format) => format.qualityLabel.includes("720") && format.hasAudio
+          )[0];
 
-        ytdl(url, {
-          format: selectedFormat,
-        })
-          .pipe(fs.createWriteStream(`./public/${name}.mp4`))
-          .on("finish", () => {
-            res.status(200).json({
-              description: `file created with name=> ${name}`,
-              formatFound: selectedFormat.qualityLabel,
-            });
+          ytdl(url, {
+            format: selectedFormat,
           })
-          .on("error", (err) => {
-            res.status(500).json({
-              description: `file created with name=> ${name}`,
-              formatFound: selectedFormat.qualityLabel,
-              err,
+            .pipe(fs.createWriteStream(`./public/${name}.mp4`))
+            .on("finish", () => {
+              res.status(200).json({
+                description: `file created with name=> ${name}`,
+                formatFound: selectedFormat.qualityLabel,
+              });
+            })
+            .on("error", (err) => {
+              res.status(500).json({
+                description: `file not created with name=> ${name}`,
+                formatFound: selectedFormat.qualityLabel,
+                err,
+              });
             });
-          });
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          description: `file not created with name=> ${name}`,
+          err,
+        });
       });
-    });
   }
 };
