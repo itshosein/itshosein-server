2b2fdbca4f5b2e4b3838e0133ce9435361f01d74
Hosein Fathi <hfathi711@gmail.com>
2022-12-10 22:49:51 +0330

    changed format

--- src/pages/api/download-yt.ts
+++ src/pages/api/download-yt.ts
@@ -6,20 +6,25 @@ type Data = {
   description: string;
 };
 
-export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   if (req.query.v) {
     const url = `http://www.youtube.com/watch?v=${req.query.v}`;
     ytdl.getBasicInfo(url).then((value) => {
       const name = `${value.videoDetails.title}`;
-      ytdl(url, {
-        quality: 136,
-      })
-        .pipe(fs.createWriteStream(`./public/${name}.mp4`))
-        .on("finish", () => {
-          res
-            .status(200)
-            .json({ description: `file created with name=> ${name}` });
-        });
+      ytdl.getInfo(req.query.v as string).then((info) => {
+        let formats = ytdl.filterFormats(info.formats, "videoandaudio");
+
+        ytdl(url, {
+          quality: 136,
+          format: formats[0],
+        })
+          .pipe(fs.createWriteStream(`./public/${name}.mp4`))
+          .on("finish", () => {
+            res
+              .status(200)
+              .json({ description: `file created with name=> ${name}` });
+          });
+      });
     });
   }
 };
