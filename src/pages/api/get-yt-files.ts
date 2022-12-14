f7b86642383f2fe96dee965c7bc03b6e9c652319
Hosein Fathi <hfathi711@gmail.com>
2022-12-14 19:33:16 +0330

    added ip for req

--- src/pages/api/get-yt-files.ts
+++ src/pages/api/get-yt-files.ts
@@ -6,6 +6,12 @@ type Data = {
 };
 
 export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  let forwarded = req.headers["x-forwarded-for"];
+  if (Array.isArray(forwarded)) {
+    forwarded = forwarded.join("");
+  }
+  const ip = forwarded ? forwarded.split(/, /)[0] : req.socket.remoteAddress;
+  console.log("ip for request is " + ip);
   const testFolder = "./public/yt/";
   const files: string[] = fs.readdirSync(testFolder);
   res.status(200).json({
