0618981158d84bc69fc8dec77ffb9ca060b11ecb
Hosein Fathi <hfathi711@gmail.com>
2022-12-15 19:42:07 +0330

    added search result to response

--- src/pages/api/search-yt.ts
+++ src/pages/api/search-yt.ts
@@ -3,7 +3,15 @@ import fs from "fs";
 import * as yt from "youtube-search-without-api-key";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const videos = await yt.search("Hallo Welt");
-  console.log("Videos:");
-  console.log(videos);
+  const videos = await yt.search(`${req.query.q}`);
+  if (videos.length) {
+    res.status(200).json({
+      resultLength: videos.length,
+      videos: videos,
+    });
+  } else {
+    res.status(500).json({
+      error: "some error happened",
+    });
+  }
 };
